dd7588564feb9c45280cbf796aa75b02dd6ee651
Alumnos <7maac7@gmail.com>
2019-10-16 16:08:23 -0300

    codigo modificado

--- app/src/debug/java/com/example/proyectomapa/Mapa.java
+++ app/src/debug/java/com/example/proyectomapa/Mapa.java
@@ -2,27 +2,41 @@ package com.example.proyectomapa;
 
 import androidx.fragment.app.FragmentActivity;
 
+import android.app.Activity;
+import android.app.Dialog;
 import android.os.Bundle;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesUtil;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.UiSettings;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+public class Mapa extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_maps);
+        setContentView(R.layout.mapa);
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
-                .findFragmentById(R.id.map);
-        mapFragment.getMapAsync(this);
+       int estado = GooglePlayServicesUtil.isGooglePlayServicesAvailable(getApplicationContext());
+       if (estado == ConnectionResult.SUCCESS){
+           SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                   .findFragmentById(R.id.map);
+           mapFragment.getMapAsync(this);
+       }
+        else{
+            Dialog mens = GooglePlayServicesUtil.getErrorDialog(estado,(Activity) getApplicationContext(), 10);
+            mens.show();
+       }
+
     }
 
 
@@ -38,10 +52,17 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
-
+        //tipo de mapa
+        mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+        //Utilidades de  google maps
+        UiSettings USet = mMap.getUiSettings();
+        USet.setZoomControlsEnabled(true);
         // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        LatLng Chile = new LatLng(-30.604494, -72.2047422);
+        mMap.addMarker(new MarkerOptions().position(sydney).title("Marcador de Chile").
+                icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_GREEN)));
+
+        float zoomnivel = 16;
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
     }
 }
